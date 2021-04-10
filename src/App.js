f89fe7eb0d334693f01206cd70621c72ad98e7a9
li-lauren <laurenlidev@gmail.com>
2021-04-10 14:41:42 -0600

    Add user reducer and actions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Counter from "./Counter";
+import { getUser } from './redux/ducks/user';
 import "./styles.css";
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  const user = useSelector((state) => state.user.user);
+  console.log(user);
+
   const count = useSelector((state) => state.counter.count);
   const voters = [
     "Bob Smith",
@@ -13,6 +23,7 @@ export default function App() {
 
   return (
     <div className="App">
+      {user && <h1> Hello, {user.firstName} </h1>}
       <h1>Redux made easy</h1>
       <h2> Total Votes: {count}</h2>
       {voters.map(voter => <Counter name={voter} />)}
